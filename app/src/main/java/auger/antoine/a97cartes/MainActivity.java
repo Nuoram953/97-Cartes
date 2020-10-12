54ce851332fc9a0d95361a67637ffd9124582e96
Nuoram <antoinemaroun@gmail.com>
2020-10-12 12:18:31 -0400

    Base code

---
+++ app/src/main/java/auger/antoine/a97cartes/MainActivity.java
@@ -0,0 +1,14 @@
+package auger.antoine.a97cartes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
